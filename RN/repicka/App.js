e0222933e4ebd1d98a6462b45ff70e085695cf38
이정원 <jeong-won@ijeong-won-ui-MacBookAir.local>
2025-05-14 23:37:51 +0900

    chore: RN 하위 저장소 .git 삭제

---
+++ RN/repicka/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>리피카</Text>
+      <Text>잘되고잇나..</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 24, fontWeight: 'bold', marginBottom: 12,
+  },
+});
+
+export default App;
