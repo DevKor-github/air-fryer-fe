9a14aa755634a718bf777d5947d6397c01043c62
halionaz <samhsj@korea.ac.kr>
2025-05-15 00:33:12 +0900

    chore: expo로 옮기기

---
+++ App.tsx
@@ -0,0 +1,24 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>리피카</Text>
+      <Text>잘되고잇나..</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 12,
+  },
+});
